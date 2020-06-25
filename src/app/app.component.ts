c024779db90a638a7afb071a8f225e8c54e3ceec
FAS <faserrao@gmail.com>
2020-06-25 10:12:11 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { APIService } from './API.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'myAmplifyProject';
+  todos: Array<any>;
+
+  constructor(private apiService: APIService) {}
+
+  createTodo() {
+    this.apiService.CreateTodo({
+        name: 'Angular',
+        description: 'testing'
+    });
+  }
+
+  async ngOnInit() {
+    this.apiService.ListTodos().then((evt) => {
+      this.todos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe((evt) => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+  }
 }
